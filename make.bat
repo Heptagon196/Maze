982090c4f59c7ec8ce4d02896519a0ab080e0494
Heptagon196 <liu1176483765@gmail.com>
2017-05-09 17:39:03 +0800

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,15 +1,15 @@
 @echo off
 cd src
 echo CPP Conio+.cpp
-g++ -c Conio+.cpp -o Conio.o --std=c++11
+g++ -c Conio+.cpp -o Conio.o --std=c++11 -O3
 echo CPP Map.cpp
-g++ -c Map.cpp --std=c++11
+g++ -c Map.cpp --std=c++11 -O3
 echo CPP Interpreter.cpp
-g++ -c Interpreter.cpp --std=c++11
+g++ -c Interpreter.cpp --std=c++11 -O3
 echo CPP InterpreterExt.cpp
-g++ -c InterpreterExt.cpp --std=c++11
+g++ -c InterpreterExt.cpp --std=c++11 -O3
 echo CPP Cal.cpp
-g++ -c Cal.cpp --std=c++11
+g++ -c Cal.cpp --std=c++11 -O3
 echo CPP main.cpp
-g++ main.cpp Map.o Conio.o Interpreter.o InterpreterExt.o Cal.o -o ../Maze --std=c++11
+g++ main.cpp Map.o Conio.o Interpreter.o InterpreterExt.o Cal.o -o ../Maze --std=c++11 -O3
 cd ..
