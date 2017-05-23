18f13e1898c6a765a35314174769352f6bbac9c3
Heptagon196 <1176483765@qq.com>
2017-05-23 20:43:36 +0800

    F**k

--- make.bat
+++ make.bat
@@ -10,6 +10,8 @@ echo CPP InterpreterExt.cpp
 g++ -c InterpreterExt.cpp --std=c++11 -O3
 echo CPP Cal.cpp
 g++ -c Cal.cpp --std=c++11 -O3
+echo CPP Bag.cpp
+g++ -c Bag.cpp --std=c++11 -O3
 echo CPP main.cpp
-g++ main.cpp Map.o Conio.o Interpreter.o InterpreterExt.o Cal.o -o ../Maze --std=c++11 -O3
+g++ main.cpp Map.o Bag.o Conio.o Interpreter.o InterpreterExt.o Cal.o -o ../Maze --std=c++11 -O3
 cd ..
