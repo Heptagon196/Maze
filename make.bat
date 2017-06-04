91fef4da18e0fad19c28d20b0c02c7d467cb9f15
Heptagon196 <1176483765@qq.com>
2017-06-04 14:45:23 +0800

    make

--- make.bat
+++ make.bat
@@ -1,5 +1,7 @@
 @echo off
 cd src
+echo CPP main.cpp
+g++ -c main.cpp -o main.o --std=c++11 -O3
 echo CPP Conio+.cpp
 g++ -c Conio+.cpp -o Conio.o --std=c++11 -O3
 echo CPP Map.cpp
@@ -12,6 +14,8 @@ echo CPP Cal.cpp
 g++ -c Cal.cpp --std=c++11 -O3
 echo CPP Bag.cpp
 g++ -c Bag.cpp --std=c++11 -O3
-echo CPP main.cpp
-g++ main.cpp Map.o Bag.o Conio.o Interpreter.o InterpreterExt.o Cal.o -o ../Maze --std=c++11 -O3
+echo CPP Creature.cpp
+g++ -c Creature.cpp --std=c++11 -O3
+echo Maze
+g++ *.o -o ../Maze --std=c++11 -O3
 cd ..
